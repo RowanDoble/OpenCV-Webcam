666167afc6e47a42ce5645487fb0af00680734fb
Rowan Doble <rowandoble@gmail.com>
2013-10-27 16:26:57 +0000

    Added user decided flip functionality

--- Git-BasicWebcam/main.cpp
+++ Git-BasicWebcam/main.cpp
@@ -1,4 +1,5 @@
 #include <opencv2\opencv.hpp>
+#include <iostream>
 
 using namespace cv;
 
@@ -6,14 +7,27 @@ int main() {
 	VideoCapture capture;
 	capture.open(0);
 	Mat frame;
+	bool flipped = false;
+
+	std::cout << "Press m to toggle image horizontal flip" << std::endl;
 
 	namedWindow("WebCam");
 
 	while(true) {
 		capture >> frame;
+
+		if (flipped)
+			flip(frame, frame, 1);
+
 		imshow("WebCam", frame);
 
 		char c = waitKey(10);
+
+		if (c == 'm') {
+			flipped = !flipped;
+			string s = (flipped) ? "Flip on" : "Flip off";
+			std::cout << s << std::endl;
+		}
 
 		if (c == 27)
 			break;
